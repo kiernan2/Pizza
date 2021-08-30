b265e8ededd810ac221a2366ee43414d724e357a
Kiernan2 <kiernan1994@gmail.com>
2021-08-30 15:57:42 -0700

    It will not accept inputs for the size and toppings though it won't work yet because there's a bug regarding pizza toppings being an object instead of a array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
-function Pizza() {
-  this.pizzaSize = 0;
-  this.pizzaToppings = [];
+function Pizza(size,toppings) {
+  this.pizzaSize = size;
+  this.pizzaToppings = toppings;
   this.pizzaCost = 0;
 }
 
 Pizza.prototype.calculateCost = function(pizza) {
   const pizzaSize = pizza.pizzaSize;
   const pizzaToppings = pizza.pizzaToppings;
-  pizza.pizzaCost = ((30*pizzaSize) + (pizzaToppings.length() * (pizzaSize * 0.3));
-
+  pizza.pizzaCost = ((30*pizzaSize) + (pizzaToppings.length() * (pizzaSize * 0.3)));
 }
+
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    const inputtedSize = $("input:radio[name=size]").val();
+    const inputtedToppings = $("input#toppings").val().split(" ");
+    let calculatedSize = 0;
+    switch (inputtedSize) {
+      case ("small"):
+        calculatedSize = 0.75;
+        break;
+      case ("medium"):
+        calculatedSize = 1;
+        break;
+      case ("large"):
+        calculatedSize = 1.5;
+        break
+      case ("double"):
+        calculatedSize = 2;
+        break
+      default:
+        calculatedSize = 1;
+    }
+    console.log(typeof inputtedToppings);
+    let newPizza = new Pizza(inputtedSize, inputtedToppings);
+    newPizza.calculateCost(newPizza);
+    console.log(newPizza)
+  })
+})
