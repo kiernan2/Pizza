d8b566fb6a46a20cd4e29b072dfca944ed5a32a1
Kiernan2 <kiernan1994@gmail.com>
2021-08-31 14:59:37 -0700

    It will now display the information about the pizza also bug and calculation fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,52 @@
 function Pizza(size,toppings) {
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
+  this.pizzaSizeMultiplier = 0;
   this.pizzaCost = 0;
 }
 
-Pizza.prototype.calculateCost = function(pizza) {
-  const pizzaSize = pizza.pizzaSize;
-  const pizzaToppings = pizza.pizzaToppings;
-  pizza.pizzaCost = ((30*pizzaSize) + (pizzaToppings.length() * (pizzaSize * 0.3)));
+Pizza.prototype.calculateCost = function() {
+  this.pizzaCost = ((30 * this.pizzaSizeMultiplier) + (this.pizzaToppings.length * (this.pizzaSizeMultiplier * 9)));
+}
+
+Pizza.prototype.calculateSize = function() {
+  let calculatedSize = 0;
+  console.log(this.pizzaSize)
+  switch (this.pizzaSize) {
+    case ("small"):
+      calculatedSize = 0.75;
+      break;
+    case ("medium"):
+      calculatedSize = 1;
+      break;
+    case ("large"):
+      calculatedSize = 1.5;
+      break
+    case ("double"):
+      calculatedSize = 2;
+      break
+    default:
+      calculatedSize = 1;
+  }
+  this.pizzaSizeMultiplier = calculatedSize;
+}
+
+// UI Logic
+function displayPizza(pizza) {
+  const output = $("div#output");
+  const htmlForPizza = "<li> You ordered a " + pizza.pizzaSize + " with " + pizza.pizzaToppings + " it costs " + pizza.pizzaCost + "</li>";
+  output.html(htmlForPizza);
 }
 
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    const inputtedSize = $("input:radio[name=size]").val();
+    const inputtedSize = $("input:radio[name=size]:checked").val();
     const inputtedToppings = $("input#toppings").val().split(" ");
-    let calculatedSize = 0;
-    switch (inputtedSize) {
-      case ("small"):
-        calculatedSize = 0.75;
-        break;
-      case ("medium"):
-        calculatedSize = 1;
-        break;
-      case ("large"):
-        calculatedSize = 1.5;
-        break
-      case ("double"):
-        calculatedSize = 2;
-        break
-      default:
-        calculatedSize = 1;
-    }
-    console.log(typeof inputtedToppings);
     let newPizza = new Pizza(inputtedSize, inputtedToppings);
-    newPizza.calculateCost(newPizza);
-    console.log(newPizza)
+    newPizza.calculateSize();
+    newPizza.calculateCost();
+    console.log(newPizza);
+    displayPizza(newPizza);
   })
 })
